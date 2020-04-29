1fb091df0f806b47d46b3e7ef4e3c72231faf6ea
Hgo0123 <hcordillot@gmail.com>
2020-04-29 14:13:23 +0200

    {modify} function parameter :wrench:

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 var $dom = document.querySelector('.root')
 
-function sayHello() {
+function sayHello(content, color) {
     const newDiv = document.createElement('div');
-    const newContent = document.createTextNode('Hello world !');
+    const newContent = document.createTextNode(content);
     newDiv.classList.add('hello_world')
+    newDiv.style.color = color;
+    newDiv.style.fontFamily = 'Cedarville Cursive, cursive';
     newDiv.appendChild(newContent)
     $dom.appendChild(newDiv)
 }
-sayHello()
+sayHello('Hello World !', 'burlywood')
